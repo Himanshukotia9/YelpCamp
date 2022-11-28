cc8a81560a05c571e56f6736f7ce8b2eb28fc242
HimanshuKotia9 <himanshukotia9@gmail.com>
2022-11-28 19:25:44 +0530

    CRUD functionality completed

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 const path = require('path');
 const mongoose = require("mongoose");
 const methodOverride = require('method-override')
-const Campground = require('./models/campground')
+const Campground = require('./models/campground');
+const campground = require('./models/campground');
 
 app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
@@ -23,13 +24,40 @@ app.get('/', (req,res) => {
     res.render('home')
 })
 
-app.get('/makecampground', async(req,res) => {
-    const camp = new Campground({title: 'My Backyard', description: 'Cheap Camping'});
-    await camp.save();
-    res.send(camp)
+app.get('/campgrounds', async(req,res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds })
+})
+app.get('/campgrounds/new',(req,res) => {
+    res.render('campgrounds/new');
+})
+app.post('/campgrounds', async(req,res) => {
+    const campground = new Campground(req.body.campground)
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
 })
 
+app.get('/campgrounds/:id', async(req,res) =>{
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', { campground });
+})
 
+app.get('/campgrounds/:id/edit', async(req,res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', { campground });
+})
+
+app.put('/campgrounds/:id', async(req,res) =>{
+    const { id } = req.params
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground }, {runValidators: true, new: true});
+    res.redirect(`/campgrounds/${campground._id}`);
+})
+
+app.delete('/campgrounds/:id', async(req,res) => {
+    const { id } = req.params;
+    const deletedCampground =await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+})
 
 app.listen(3000,() => {
     console.log('Starting Yelp Camp on 3000')
--- seeds/index.js
+++ seeds/index.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 const methodOverride = require('method-override')
 const Campground = require('../models/campground')
 const cities = require('./cities')
+const {places, descriptors} = require('./seedHelpers');
+const { title } = require('process');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
 .then(() => {
@@ -15,16 +17,22 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
     console.log(err)
 })
 
+const sample = array => array[Math.floor(Math.random() * array.length)]
+
+
 const seedDB = async() =>{
     await Campground.deleteMany({});
     for(let i= 0; i < 50; i++){
         const random400 = Math.floor(Math.random() * 400);
         const camp = new Campground({
-            location: `${cities[random400].city}, ${cities[random400].state}`
+            location: `${cities[random400].city}, ${cities[random400].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
         })
         await camp.save();
 
     }
 }
 
-seedDB();
+seedDB().then(() =>{
+    mongoose.Connection.close();
+})
