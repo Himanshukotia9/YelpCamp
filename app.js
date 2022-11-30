f46dbe0517e246125f496182d1b326943c4fc419
HimanshuKotia9 <himanshukotia9@gmail.com>
2022-11-30 12:39:18 +0530

    Boilerplate Update

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require("mongoose");
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override')
 const Campground = require('./models/campground');
 const campground = require('./models/campground');
@@ -10,6 +11,7 @@ app.set('views', path.join(__dirname,'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
+app.engine('ejs',ejsMate)
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
 .then(() => {
